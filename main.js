9ab4875a5dd0c117a2b53df9a7edab8489beb584
ahmedatif99 <ahmedahmed1919a@gmail.com>
2023-04-17 16:47:34 +0300

    product, product review sections, and fixing some responsive issue

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+  new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 3,
+    autoplay: {
+      delay: 8000,
+    },
+    
+  })
+})
